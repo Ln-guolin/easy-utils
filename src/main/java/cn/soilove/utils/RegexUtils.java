ce72f4cc47f96f661845c8fa1d21387be6f63a7e
chenguolin <chenguolin@meeruu.com>
2020-07-15 09:59:30 +0800

    升级工具包

--- src/main/java/cn/soilove/utils/RegexUtils.java
+++ src/main/java/cn/soilove/utils/RegexUtils.java
@@ -43,13 +43,14 @@ public class RegexUtils {
 	
 
 	/**
+	 * 执行器
 	 * @param regex
 	 *            正则表达式字符串
 	 * @param str
 	 *            要匹配的字符串
 	 * @return 如果str 符合 regex的正则表达式格式,返回true, 否则返回 false;
 	 */
-	private static boolean match(String regex, String str) {
+	public static boolean match(String regex, String str) {
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(str);
 		return matcher.matches();
